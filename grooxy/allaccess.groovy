86adbe1bc49d425e1cf6735483d416378f500434
fraseracct <fraseracct@gmail.com>
2018-05-02 07:27:07 -0400

    Create allaccess.groovy

---
+++ grooxy/allaccess.groovy
@@ -0,0 +1,11 @@
+/*
+ * input: ServletRequest request
+ * input: ServletResponse response
+ * input: FilterChain chain
+ * 
+ * output: Set<String> adiRoles
+ * output: String username
+ */
+
+username="BillyBob"
+adiRoles.add("User")
